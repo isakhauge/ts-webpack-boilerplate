de695059414e3564026ef9d12cf24259e8eb5c17
isakhauge <isak.hauge@fdvhuset.no>
2021-01-10 21:23:50 +0100

    Add WebpackBundleAnalyzer for enhanced dev workflow. Remove unused packages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,51 @@
 const path = require('path')
+const BundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const relPath = relativePath => path.resolve(__dirname, relativePath)
 
-module.exports = {
-    mode: "development",
-    entry: relPath('src/App.ts'),
-    output: {
-        path: relPath('dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.ts']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ['@babel/preset-env', '@babel/preset-typescript']
+module.exports = env => {
+    const analyzeBundles = env?.BUNDLE_ANALYZER === 'true'
+    return {
+        entry: relPath('src/App.ts'),
+        output: {
+            path: relPath('dist'),
+            filename: 'bundle.js'
+        },
+        resolve: {
+            extensions: ['.js', '.ts']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js)$/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {
+                                presets: ['@babel/preset-env', '@babel/preset-typescript']
+                            }
                         }
-                    }
-                ],
-                include: relPath('src')
-            },
-            {
-                test: /\.(ts)$/,
-                use: ['ts-loader'],
-                include: relPath('src')
+                    ],
+                    include: relPath('src')
+                },
+                {
+                    test: /\.(ts)$/,
+                    use: ['ts-loader'],
+                    include: relPath('src')
+                }
+            ]
+        },
+        plugins: ((analyze, plugins) => {
+            if (analyze) {
+                plugins.push(new BundleAnalyzer())
             }
-        ]
-    },
-    devServer: {
-        contentBase: relPath('dist'),
-        port: 9000,
-        hot: true
+            return plugins
+        })(analyzeBundles,[
+            // Your plugins here.
+        ]),
+        devServer: {
+            contentBase: relPath('dist'),
+            port: 9000,
+            hot: true
+        }
     }
 }
